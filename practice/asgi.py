a55922c9b77bb361ca1580f6322b2540ce3dbba4
Vivek Tiwari <vivektiwaricod@gmail.com>
2023-12-25 22:53:44 +0530

    added websocket with scaling

--- practice/asgi.py
+++ practice/asgi.py
@@ -1,16 +1,29 @@
-"""
-ASGI config for practice project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
+import django
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels_redis.core import RedisChannelLayer
+from channels.layers import get_channel_layer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice.settings')
+django.setup()
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from websocket.routing import websocket_urlpatterns
+
+
+websocket_routing = URLRouter(
+       websocket_urlpatterns
+)
+
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        websocket_routing
+    )
+
+})
+
+
+
