996bb6e1f403f350332d1650f8c40b6dcc960006
Vivek Tiwari <vivektiwaricod@gmail.com>
2023-12-28 21:43:22 +0530

    celery is added and containerized

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse( "<html><body><h1>Practice Project</h1></body></html>")
